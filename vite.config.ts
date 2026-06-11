59c902e402b07f93a2f7816325cf1c384457e69f
r.s. <richter.s.94@web.de>
2026-06-11 15:25:00 +0200

    Add Slash-Sign in Basename in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "3D-Slider-Uebung",
+  base: "/3D-Slider-Uebung/",
   plugins: [react()],
 });
